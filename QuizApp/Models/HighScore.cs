3ad75ffec8fea7458b26b25b5570059eaffcebb6
Kaushal Soni <91665722+KaushalSony@users.noreply.github.com>
2025-04-19 12:53:26 +0530

    Added quiz logic

--- QuizApp/Models/HighScore.cs
+++ QuizApp/Models/HighScore.cs
@@ -1,9 +1,10 @@
-namespace QuizApp.Models
-{
+using System;
+
+namespace QuizApp.Models;
+
     public class HighScore
     {
         public string Name { get; set; }
         public int Score { get; set; }
         public DateTime Date { get; set; }
     }
-}
