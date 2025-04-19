458ca00bae048f1a15dbdecd2a826cce4164dd82
MAHAK GUPTA <mahakgupta092@gmail.com>
2025-04-19 11:27:34 +0530

    Resolved conflict and updated .gitignore

---
+++ QuizApp/Models/HighScore.cs
@@ -0,0 +1,9 @@
+namespace QuizApp.Models
+{
+    public class HighScore
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
